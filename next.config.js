615cea656ddd7f8a3bbd227fa6fda711ad0d9b65
Montier Elliott <montier.elliott@bofa.com>
2023-03-18 20:52:00 -0400

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+/**
+ * @module next.config
+ *
+ *
+ * @author montier.elliott@gmail.com
+ * @description NextJS configuration
+ */
+
+/**
+ * Configuration object for the current NextJS application
+ *
+ *
+ * @type {NextConfig}
+ */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+/** exporting */
+module.exports = nextConfig;
