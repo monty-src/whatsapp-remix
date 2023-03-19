8584059afbff7c5726f466ca132ff3cc460edcc8
Montier Elliott <montier.elliott@bofa.com>
2023-03-19 10:43:55 -0400

    Dev added user avatar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,11 @@
 import React from "react";
 
 import styled from "styled-components";
+import { signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Avatar, Button, IconButton } from "@mui/material";
+
+import { auth } from "../firebase";
 
 /**
  * Sidebar component
@@ -16,9 +21,14 @@ import styled from "styled-components";
  * @returns {JSX.Element}
  */
 const Sidebar = (): JSX.Element => {
+  const [user] = useAuthState(auth);
+  const signUserOut = () => signOut(auth);
+
   return (
     <Container>
-      <p>testing something rightn ow</p>
+      <Header>
+        <UserAvatar src={user?.photoURL as string} onClick={signUserOut} />
+      </Header>
     </Container>
   );
 };
@@ -29,6 +39,29 @@ const Sidebar = (): JSX.Element => {
  *
  */
 const Container = styled.div``;
+
+const Header = styled.div`
+  top: 0;
+  z-index: 1;
+  position: sticky;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 80px;
+  padding: 15px;
+
+  background-color: #fff;
+  border-bottom: 1px solid #f5f5f5;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
 
 /** exporting */
 export default Sidebar;
